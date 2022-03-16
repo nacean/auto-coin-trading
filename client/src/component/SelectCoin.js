6be0615020fdba59da0369349c57107d3a1333f8
nacean <nacean@naver.com>
2022-03-17 02:21:10 +0900

    analyze interval 켜기,끄기 추가, Coin 이름 오름차순 정리

--- client/src/component/SelectCoin.js
+++ client/src/component/SelectCoin.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from "react";
+import analyzeCandle from "../modules/analyzeCandle";
 import marketCode from "../modules/marketCode";
-import AnalyzeCandle from "./AnalyzeCandle";
 import CoinOption from "./CoinOption";
 
 function SelectCoin() {
+  const [working, setworking] = useState(null);
   const [coinList, setcoinList] = useState([]);
-  const [selectedCoin, setSelectedCoin] = useState({
-    market: "KRW-BTC",
-    korean_name: "비트코인",
-  });
+  const [selectedCoin, setSelectedCoin] = useState({});
 
   useEffect(() => {
+    //마켓코드, 첫 코인 초기화
     marketCode().then((response) => {
-      const refineCoinList = response.marketCode.filter(({ market }) => {
+      let refineCoinList = response.marketCode.filter(({ market }) => {
         return market.indexOf("KRW") !== -1;
       });
+      refineCoinList = refineCoinList.sort((a, b) => {
+        if (a.korean_name > b.korean_name) return 1;
+        else if (a.korean_name < b.korean_name) return -1;
+        else return 0;
+      });
       setcoinList(refineCoinList);
+      const firstCoin = {
+        market: refineCoinList[0].market,
+        korean_name: refineCoinList[0].korean_name,
+      };
+      setSelectedCoin(firstCoin);
+      return refineCoinList[0];
     });
   }, []);
 
@@ -38,7 +48,25 @@ function SelectCoin() {
         })}
       </select>
       <div>현재 선택하신 코인은 [{selectedCoin.korean_name}] 입니다</div>
-      <AnalyzeCandle Coin={selectedCoin} />
+      {working === null ? (
+        <button
+          onClick={() => {
+            const timer = analyzeCandle(selectedCoin);
+            setworking(timer);
+          }}
+        >
+          시작하기
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            clearInterval(working);
+            setworking(null);
+          }}
+        >
+          그만하기
+        </button>
+      )}
     </div>
   );
 }
