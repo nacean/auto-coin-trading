1cd068a650706947e18ed37bea42f9398bacb332
nacean <nacean@naver.com>
2022-03-19 01:47:25 +0900

    purchaseInfo 를 통해 체결당시 가격,볼륨 확인

--- client/src/modules/analyzeSellingPoint.js
+++ client/src/modules/analyzeSellingPoint.js
@@ -1,35 +1,56 @@
 import purchaseStore from "../utils/purchaseStore";
 import orderCoin from "./orderCoin";
+import purchaseInfo from "./purchaseInfo";
 import ticker from "./ticker";
-
+let cnt = 0;
 function analyzeSellingPoint(market) {
   const { getPurchasedCoin, setpurchasedCoin, resetCoin } = purchaseStore;
-  const timer = setInterval(() => {
-    orderCoin(market, "bid", "0").then((response) => {
-      const newPurchasedCoin = response.order;
-      setpurchasedCoin(newPurchasedCoin);
+
+  orderCoin(market, "bid", "0").then((response) => {
+    const newPurchasedCoin = response.order;
+    console.log("구매하였습니다", response.order);
+    const { uuid } = newPurchasedCoin;
+
+    purchaseInfo(uuid).then((response) => {
+      // trades에 필요한거 다 있음
+      //info.trades[0].price : 평매수가
+      //info.trades[0].volume : 매수량
+      const info = response.info;
+      setpurchasedCoin(info);
+      const trades = getPurchasedCoin().trades[0];
+      const { price, volume } = trades;
+
+      const timer = setInterval(() => {
+        ticker(market).then((response) => {
+          const { trade_price } = response.ticker[0];
+          console.log(
+            `${cnt} : price is ${price}, trade price is ${trade_price}`
+          );
+          //if (price * 1.01 <= trade_price) {
+          if (price * 1.005 <= trade_price) {
+            //조건완화
+            //수익이 나는 경우
+            orderCoin(market, "ask", volume).then((response) => {
+              console.log("수익이 발생했습니다", response.order);
+              clearInterval(timer);
+            });
+          } //else if (trade_price * 1.01 <= price) {
+          else if (trade_price * 1.005 <= price) {
+            //조건완화
+            //수익이 안나는 경우
+            orderCoin(market, "ask", volume).then((response) => {
+              console.log("손실이 발생했습니다.", response.order);
+              clearInterval(timer);
+            });
+          }
+        });
+        cnt++;
+      }, 1000);
+
+      //코인 초기화
+      resetCoin();
     });
-
-    const purchasedCoin = getPurchasedCoin();
-    const { price } = purchasedCoin;
-
-    ticker(market).then((response) => {
-      const { opening_price, executed_volume } = response.ticker;
-      if (price * 1.01 <= opening_price) {
-        //수익이 나는 경우
-        orderCoin(market, "ask", executed_volume);
-        console.log("수익이 발생했습니다");
-        clearInterval(timer);
-      } else if (opening_price * 1.015 <= price) {
-        //수익이 안나는 경우
-        orderCoin(market, "ask", executed_volume);
-        console.log("손실이 발생했습니다.");
-        clearInterval(timer);
-      }
-    });
-  }, 1000);
-
-  resetCoin();
+  });
 }
 
 export default analyzeSellingPoint;
