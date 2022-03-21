a9a128a251815f17f4e06923acc6717d930010ce
nacean <nacean@naver.com>
2022-03-21 21:51:39 +0900

    doingPurchase 이용, false일때만 analyzeCandle interval 내 내용 작동하게 변경

--- client/src/modules/analyzeCandle.js
+++ client/src/modules/analyzeCandle.js
@@ -1,7 +1,9 @@
 import minuteCandle from "./minuteCandle";
 import candleStore from "../utils/candleStore";
 import analyzeSellingPoint from "./analyzeSellingPoint";
+import purchaseStore from "../utils/purchaseStore";
 function analyzeCandle(Coin) {
+  const { getdoingPurchase } = purchaseStore;
   const {
     getTopCandle,
     getDownCandles,
@@ -16,56 +18,59 @@ function analyzeCandle(Coin) {
   resetCandle();
 
   const timer = setInterval(() => {
-    minuteCandle(market).then((response) => {
-      const newCandleInfo = response.minuteCandle[0];
-      console.log(newCandleInfo);
-      const { opening_price, trade_price } = newCandleInfo;
-      const candleDegree = trade_price - opening_price;
+    const doingPurchase = getdoingPurchase();
+    if (!doingPurchase) {
+      minuteCandle(market).then((response) => {
+        const newCandleInfo = response.minuteCandle[0];
+        console.log(newCandleInfo);
+        const { opening_price, trade_price } = newCandleInfo;
+        const candleDegree = trade_price - opening_price;
 
-      if (getTopCandle() === null) {
-        //첫 양봉 갱신
-        if (candleDegree > 0) {
-          initTopCandle(newCandleInfo);
-          resetCandle();
-          console.log("양봉 갱신");
-        }
-      } else {
-        //topCandle보다 큼 or 빅 양봉임
-        if (
-          trade_price > getTopCandle().trade_price ||
-          candleDegree >= trade_price * 0.005
-        ) {
-          console.log("더 큰 양봉 발견 or 빅 양봉");
-          initTopCandle(newCandleInfo);
-          resetCandle();
+        if (getTopCandle() === null) {
+          //첫 양봉 갱신
+          if (candleDegree > 0) {
+            initTopCandle(newCandleInfo);
+            resetCandle();
+            console.log("양봉 갱신");
+          }
         } else {
-          const comparePrice =
-            getDownCandles().length === 0
-              ? getTopCandle().trade_price
-              : getDownCandles()[getDownCandles().length - 1].trade_price;
-          console.log(
-            `compare Price : ${
-              comparePrice - comparePrice * 0.009
-            } , trade_price : ${trade_price}`
-          );
-          if (comparePrice - comparePrice * 0.009 > trade_price) {
+          //topCandle보다 큼 or 빅 양봉임
+          if (
+            trade_price > getTopCandle().trade_price ||
+            candleDegree >= trade_price * 0.005
+          ) {
+            console.log("더 큰 양봉 발견 or 빅 양봉");
+            initTopCandle(newCandleInfo);
+            resetCandle();
+          } else {
+            const comparePrice =
+              getDownCandles().length === 0
+                ? getTopCandle().trade_price
+                : getDownCandles()[getDownCandles().length - 1].trade_price;
+            console.log(
+              `compare Price : ${
+                comparePrice - comparePrice * 0.009
+              } , trade_price : ${trade_price}`
+            );
+            /*if (comparePrice - comparePrice * 0.009 > trade_price) {
             insertCandle(newCandleInfo);
             console.log("음봉 1틱 추가");
-          }
+          }*/
+            if (comparePrice - comparePrice * 0.001 > trade_price) {
+              //테스트를 위해 조건 완화
+              insertCandle(newCandleInfo);
+              console.log("음봉 1틱 추가");
+            }
 
-          if (getDownCandles().length === 3) {
-            console.log("매수가 시작되었습니다.");
-            //3틱 매집, 매수 시작
-            analyzeSellingPoint(market);
-            resetCandle();
-            initTopCandle(null);
-            console.log("매도가 실행되고 종료되었습니다.");
+            if (getDownCandles().length === 3) {
+              console.log("매수가 시작되었습니다.");
+              //3틱 매집, 매수 시작
+              analyzeSellingPoint(market);
+            }
           }
         }
-      }
-
-      console.log(candleStore);
-    });
+      });
+    }
   }, 3000);
 
   return timer;
--- client/src/modules/analyzeSellingPoint.js
+++ client/src/modules/analyzeSellingPoint.js
@@ -1,10 +1,15 @@
+import candleStore from "../utils/candleStore";
 import purchaseStore from "../utils/purchaseStore";
 import orderCoin from "./orderCoin";
 import purchaseInfo from "./purchaseInfo";
 import ticker from "./ticker";
 let cnt = 0;
 function analyzeSellingPoint(market) {
-  const { getPurchasedCoin, setpurchasedCoin, resetCoin } = purchaseStore;
+  const { getPurchasedCoin, setpurchasedCoin, resetCoin, setdoingPurchase } =
+    purchaseStore;
+  const { initTopCandle, resetCandle } = candleStore;
+
+  setdoingPurchase(true);
 
   orderCoin(market, "bid", "0").then((response) => {
     const newPurchasedCoin = response.order;
@@ -27,19 +32,29 @@ function analyzeSellingPoint(market) {
             `${cnt} : price is ${price}, trade price is ${trade_price}`
           );
           //if (price * 1.01 <= trade_price) {
-          if (price * 1.005 <= trade_price) {
+          if (price * 1.01 <= trade_price) {
             //조건완화
             //수익이 나는 경우
             orderCoin(market, "ask", volume).then((response) => {
               console.log("수익이 발생했습니다", response.order);
+              //캔들 초기화
+              initTopCandle(null);
+              resetCandle();
+
+              setdoingPurchase(false); //purchase 끝났다는걸 analyzeCandle interval에 알림
               clearInterval(timer);
             });
           } //else if (trade_price * 1.01 <= price) {
-          else if (trade_price * 1.005 <= price) {
+          else if (trade_price * 1.01 <= price) {
             //조건완화
             //수익이 안나는 경우
             orderCoin(market, "ask", volume).then((response) => {
               console.log("손실이 발생했습니다.", response.order);
+              //캔들 초기화
+              initTopCandle(null);
+              resetCandle();
+
+              setdoingPurchase(false); //purchase 끝났다는걸 analyzeCandle interval에 알림
               clearInterval(timer);
             });
           }
